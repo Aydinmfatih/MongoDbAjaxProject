c19a40d53a99df9843fdc5aa432982d9c18830aa
aydinmfatih <aydinmfatih@gmail.com>
2023-12-14 21:20:20 +0300

    updated

---
+++ MongoDbAjaxProject/DAL/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDbAjaxProject.DAL.Entities
+{
+    public class Employee
+    {
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeSurname { get; set; }
+        public decimal EmployeeSalary { get; set; }
+    }
+}
